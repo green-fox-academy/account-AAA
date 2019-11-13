d66451623b8fff91fddb829355a5dd4a1f9b8cf4
大多 <eleven032@gmail.com>
2019-11-13 17:15:36 +0800

    doing createnewdeposite

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { CreateNewDeposit } from './components/CreateNewDeposit';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CreateNewDeposit />
     </View>
   );
 }
---
+++ frontend/components/CreateNewDeposit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function CreateNewDeposit() {
+    return (
+        <View>
+            <Text>Name of deposit account</Text>
+        </View>
+    )
+}
+
+export default { CreateNewDeposit };
