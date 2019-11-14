b25d8e83d1a92fff0727b4ffa809683b254928c0
大多 <eleven032@gmail.com>
2019-11-14 12:43:29 +0800

    app structure modified
    some small change
    
    env(frontend): add ignore .env file
    
    Create expo repo
    
    fix: create expo frontend folder with basic structure
    
    env(frontend): format change

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { CreateNewDeposit } from './components/CreateNewDeposit';
 
 export default function App() {
---
+++ frontend/src/components/CreateNewDeposite.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+function CreateNewDeposite() {
+    return (
+        <View>
+            <Text>
+            
+                This is a place holder!!!
+            </Text>
+        </View>)
+}
