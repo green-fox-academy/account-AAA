3bac6ac237fe87efbc96f7ad919f5622d3d58f02
Hao <yuhao3570@gmail.com>
2019-11-14 17:40:54 +0800

    fix(pkg): fix npm scripts for windows

--- frontend/src/components/HeaderBar.js
+++ frontend/src/components/HeaderBar.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-import {  } from 'react-native';
+import { } from 'react-native';
 
-export default function HeaderBar () {
-
+export default function HeaderBar() {
   return (
     <></>
-  )
-} 
+  );
+}
