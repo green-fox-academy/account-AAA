2b06c5651ad1a6d6f81ed92f54b5df9a2e13d2b9
Cathy Chen <x566chen@uwaterloo.ca>
2019-11-15 11:42:32 +0800

    express(backend):update

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,28 +1,33 @@
 const express = require('express');
+
 const router = express.Router();
 
-router.post('/',(req, res)=>{
-  let {deposit_name, user_id} = req.body;
-  if(req.headers["content-type"]!=='application/json'){
+router.post('/', (req, res) => {
+  const {deposit_name, user_id} = req.body;
+  if (req.headers['content-type'] !== 'application/json') {
     res.status(415).json({
-          "message": "Content-type must be application/json."
-      })
+      "message": 'Content-type must be application/json.',
+    });
+    return;
   }
-  let req_body_keys = Object.keys(req.body);
-  let curr_key = '';
-  req_body_keys.forEach(e=>{
-    if (!req_body_keys.includes(e)){
-      curr_key = curr_key + `<${e}>`;
+  const reqBodykeys = Object.keys(req.body);
+  let missingKeys = '';
+  const keysArr = ['deposit_name', 'user_id'];
+  keysArr.forEach((item) => {
+    if (!reqBodykeys.includes(item)) {
+      missingKeys += `${item}`;
     }
-  })
-  if (str.length!=0){
+  });
+  if (missingKeys.length !== 0) {
     res.status(400).json({
-      "message": `Missing ${curr_key}.`
-  })
+      "message": `Missing ${missingKeys}.`,
+    });
+    return;
   }
-    res.status(200).json({
-        "id": user_id,
-    })
-})
-//leave space for Internal Server Error
+  res.status(200).json({
+    "id": user_id,
+  });
+  return;
+});
+// leave space for Internal Server Error
 module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
-let express = require('express');
-let routes = require('./routes/routes');
-let app = express();
+const express = require('express');
+const routes = require('./routes/routes');
+
+const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 app.use('/deposit', routes);
 
 app.listen(port, () => {
-    /* eslint-disable no-alert, no-console */
-      console.log(`we are in port: ${port}`);
-    /* eslint-enable no-alert */
-    });
+  /* eslint-disable no-alert, no-console */
+  console.log(`we are in port: ${port}`);
+  /* eslint-enable no-alert */
+});
