f4c871c4550adbaa897244ce7b73ae4b4570ea4c
大多 <eleven032@gmail.com>
2019-11-18 09:28:13 +0800

    fix(frontend): seperate the style sheet

--- frontend/src/components/CreateNewDeposit.js
+++ frontend/src/components/CreateNewDeposit.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import { View, KeyboardAvoidingView } from 'react-native';
 import {
   Button, Item, Input, Text, Icon,
 } from 'native-base';
+import styles from '../styles/CreateNewDepositStyle';
+import buttonStyle from '../styles/BottomButtonStyle';
 
-
-const styles = StyleSheet.create({
-  containerMain: {
-    flex: 1,
-  },
-  text: {
-    fontSize: 25,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-  },
-  inputText: {
-    margin: 20,
-  },
-  button: {
-    marginBottom: 50,
-    marginRight: 30,
-    width: 120,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-  },
-});
 
 function CreateNewDeposit() {
   return (
@@ -42,7 +19,7 @@ function CreateNewDeposit() {
         </Item>
       </View>
       <View style={{ flex: 6, flexDirection: 'column-reverse', alignItems: 'flex-end' }}>
-        <Button style={styles.button} iconLeft rounded warning>
+        <Button style={buttonStyle.button} iconLeft rounded warning>
           <Icon name="add" />
           <Text>Create</Text>
         </Button>
---
+++ frontend/src/styles/BottomButtonStyle.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  button: {
+    marginBottom: 50,
+    marginRight: 30,
+    width: 120,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+  },
+});
