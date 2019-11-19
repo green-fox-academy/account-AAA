a0092976ccb2ea534c4b99030474b06c4ef41303
Hao <yuhao3570@gmail.com>
2019-11-19 12:09:57 +0800

    fix(env): add in dependacies

--- backend/db/migrations/1574132487458_accounts.js
+++ backend/db/migrations/1574132487458_accounts.js
@@ -1,4 +1,8 @@
 module.exports = {
-  up: '',
-  down: '',
+  up: 'CREATE TABLE IF NOT EXISTS accounts ('
+      + 'id INT PRIMARY KEY AUTO_INCREMENT, '
+      + 'deposit_name VARCHAR(255) NOT NULL, '
+      + 'deposit_amount BIGINT DEFAULT 0, '
+      + 'user_id INT);',
+  down: 'DROP TABLE accounts;',
 };
