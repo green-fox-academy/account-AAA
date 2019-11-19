a7cad488df7677736530a40eb07a0fa7171dacbc
Hao <yuhao3570@gmail.com>
2019-11-19 14:55:08 +0800

    fix(style): change string quotation to single quote

--- backend/db/migrations/1574145249159_accounts.js
+++ backend/db/migrations/1574145249159_accounts.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "up": "INSERT INTO accounts(deposit_name, user_id) "
-      + "VALUES "
-      + "(\"Main Account\", 123), "
-      + "(\"Saving Account\", 123), "
-      + "(\"Investment Account\", 123); ",
-  "down": "TRUNCATE TABLE accounts;"
-}
+  up: 'INSERT INTO accounts(deposit_name, user_id) '
+      + 'VALUES '
+      + '("Main Account", 123), '
+      + '("Saving Account", 123), '
+      + '("Investment Account", 123); ',
+  down: 'TRUNCATE TABLE accounts;',
+};
