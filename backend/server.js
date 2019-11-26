3890b62a1b4c63d6b52514191d97220f4f5661b6
Hao <45909402+yuhao3570@users.noreply.github.com>
2019-11-26 16:46:57 +0800

    code(JSAP-11): added Swagger api doc for express endpoints (#19)
    * code(JSAP-11): added Swagger api doc for express endpoints
    
    * perf(JSAP-11): add more account row
    
    * perf(JSAP-11): gathered doc and edited accounts seed

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const OAuth = require('./src/MiddleWare/OAuth');
+const swaggerSpec = require('./swaggerSpec');
 const depositRoute = require('./src/routes/depositRoute');
 
 const app = express();
 const port = process.env.PORT;
+
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/deposit', OAuth, depositRoute);
 
 app.listen(port, () => {
--- backend/src/routes/depositRoute.js
+++ backend/src/routes/depositRoute.js
@@ -44,5 +44,4 @@ depositRoute.get('/', async (req, res) => {
   }
 });
 
-
 module.exports = depositRoute;
