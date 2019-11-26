70c5ccad1d42a022477727de583da348df79f336
大多 <eleven032@gmail.com>
2019-11-26 13:45:38 +0800

    backend(RDS for AWS): first test
    backend(RDS PM2): ADD TIMEOUT FEATURES IN DB CONNECTION
    
    backend(JSAP-38): finish rds and aws part
    
    backend(JSAP-38):Updated readme file
    
    backend(RDS): combine database migration into script
    
    backend(PM2): test bug
    
    backend(PM2): test bug
    
    backend(PM2): test bug
    
    backend(PM2): test bug
    
    backend(RDS):test bug
    
    backend(RDS):test bug
    
    backend(RDS):find bug and test
    
    backend(RDS):find bug and test
    
    backend(RDS):test and search bug
    
    backend(RDS):test and search bug
    
    backend(RDS):test and search bug
    
    backend(RDS):create fresh new account-AAA
    
    backend(RDS):search bug
    
    backend(RDS):search bug
    
    backend(RDS):search bug
    
    backend(RDS):search bug
    
    backend(RDS):search bug
    
    backend(RDS):search bug
    
    backend(RDS):search bug
    
    backend(RDS):search bug
    
    backend(RDS):search bug
    
    backend(RDS):search bug
    
    backend(RDS):test

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   apps: [{
     name: 'backend',
-    script: 'src/server.js',
+    script: 'server.js',
     env: {
       NODE_ENV: 'development',
     },
     env_production: {
       NODE_ENV: 'production',
-      PORT: 3000,
     },
   }],
 
@@ -16,10 +15,10 @@ module.exports = {
       user: 'ubuntu',
       host: '3.112.128.181',
       key: './.ssh/JSA-aaa-kp.pem',
-      ref: 'origin/master',
+      ref: 'origin/JSAP-38',
       repo: 'git@github.com:green-fox-academy/account-AAA.git',
       path: '/home/ubuntu/account-AAA',
-      'post-deploy': 'cd backend && npm install && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd backend && npm install && pm2 startOrRestart ecosystem.config.js --update-env && npm run dbRefresh',
     },
   },
 };
