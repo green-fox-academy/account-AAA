87da3de7cd7121ebe849747bbf5afd912638b71c
Hao <yuhao3570@gmail.com>
2019-11-26 11:37:01 +0800

    code(JSAP-11): added Swagger api doc for express endpoints

--- backend/src/routes/depositRoute.js
+++ backend/src/routes/depositRoute.js
@@ -6,6 +6,44 @@ const DepositRequestValidator = require('../services/DepositRequestValidator');
 const databaseActions = new DatabaseActions(connection);
 const depositRequestValidator = new DepositRequestValidator();
 
+/**
+ * @swagger
+ * /deposit:
+ *   post:
+ *     tags:
+ *       - Deposits
+ *     name: Create Deposit Account
+ *     summary: reate a new account under the caller user
+ *     security:
+ *       - bearerAuth: []
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: depositName
+ *         description: Name of deposit account to be created.
+ *         schema:
+ *           type: object
+ *           required:
+ *             - depositName
+ *           properties:
+ *             depositName:
+ *               type: string
+ *     responses:
+ *       '200':
+ *         description: User found and deposit account created successfully
+ *       '401':
+ *         description: Provided token is invalid
+ *       '415':
+ *         description: No auth token provided or provides token is not of Bearer type
+ *       '422':
+ *         description: Duplicate depositName
+ *       '500':
+ *         description: DB problems
+ */
+
 depositRoute.post('/', async (req, res) => {
   if (!depositRequestValidator.validateContentType(req.headers['content-type'])) {
     res.status(415).send('Content-type must be application/json.');
@@ -34,6 +72,29 @@ depositRoute.post('/', async (req, res) => {
     res.status(500).send('Something went wrong, please try again later.');
   }
 });
+
+/**
+ * @swagger
+ * /deposit:
+ *   get:
+ *     tags:
+ *       - Deposits
+ *     name: Retrive deposit accounts
+ *     summary: Retrive all deposit accounts under callers Id
+ *     security:
+ *       - bearerAuth: []
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       '200':
+ *         description: User found and deposit account sent back successfully
+ *       '415':
+ *         description: No auth token provided or provides token is not of Bearer type
+ *       '401':
+ *         description: Provided token is invalid
+ *       '500':
+ *         description: DB problems
+ */
 
 depositRoute.get('/', async (req, res) => {
   try {
