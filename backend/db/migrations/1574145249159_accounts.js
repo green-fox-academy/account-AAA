ea4b130d7c77c7fc0bca74151461aabdfc042262
Hao <yuhao3570@gmail.com>
2019-11-26 14:14:03 +0800

    perf(JSAP-11): add more account row

--- backend/db/migrations/1574145249159_accounts.js
+++ backend/db/migrations/1574145249159_accounts.js
@@ -3,6 +3,10 @@ module.exports = {
       + 'VALUES '
       + '("Main Account", 123), '
       + '("Saving Account", 123), '
-      + '("Investment Account", 123); ',
+      + '("Investment Account", 123), '
+      + '("Salary", 456), '
+      + '("Pocket Money", 456), '
+      + '("Investment Account", 456), '
+      + '("Stock Account", 456)',
   down: 'TRUNCATE TABLE accounts;',
 };
