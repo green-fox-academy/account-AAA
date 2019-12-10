e8a7bb98c2db55efa94b24c7bab7c12ba47382bf
大多 <eleven032@gmail.com>
2019-12-10 11:18:00 +0800

    JSAP-60:REABSE MASTER

--- frontend/src/components/AccountInfo.js
+++ frontend/src/components/AccountInfo.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { CardItem, Text } from 'native-base';
+import { CardItem, Text, Button, View } from 'native-base';
+import { TextInput } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import Modal, {
+  ModalTitle,
+  ModalContent,
+  ModalFooter,
+  ModalButton,
+  SlideAnimation,
+} from 'react-native-modals';
 import accountPropTypes from '../helpers/accountPropTypes';
 import styles from '../styles/AccountInfoStyle';
 
@@ -7,11 +16,56 @@ export default function AccountInfo({ account }) {
   const {
     id, depositName, interestRate, depositAmount,
   } = account;
+
+  const [visible, setVisible] = React.useState(false);
+  const [accountName, setAccountName] = React.useState('');
+
+  const handleChange = (text) => {
+    setAccountName(text);
+  };
+ 
+
+
   return (
     <>
       <CardItem style={styles.accountInfo}>
         <Text style={styles.depositId}>{`ID ${id}`}</Text>
-        <Text numberOfLines={1} style={styles.depositName}>{`${depositName}`}</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: 300 }}>
+          <Text numberOfLines={1} style={styles.depositName}>{`${depositName}`}</Text>
+          <Button footer onPress={() => setVisible(true)} transparent>
+            <FontAwesome name="edit" size={18} />
+          </Button>
+          <Modal
+            visible={visible}
+            width={0.9}
+            overlayOpacity={1}
+            rounded
+            actionsBordered
+            modalAnimation={new SlideAnimation({
+              slideFrom: 'bottom',
+            })}
+            modalTitle={<ModalTitle title="Edit your account name" />}
+            footer={
+              <ModalFooter>
+                <ModalButton
+                  text="CANCEL"
+                  onPress={() => { setVisible(false) }}
+                />
+                <ModalButton
+                  text="OK"
+                  onPress={() => { setVisible(false) }}
+                />
+              </ModalFooter>
+            }
+          >
+            <ModalContent>
+              <TextInput
+                onChangeText={(text) => handleChange(text)}
+                placeholder="Type here"
+              />
+            </ModalContent>
+          </Modal>
+        </View>
         <Text style={styles.interestRate}>{`current interest rate: ${interestRate}`}</Text>
       </CardItem>
 
