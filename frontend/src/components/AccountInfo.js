e33db434192fb235e0a40e1c1929b136731ba41a
Hao <yuhao3570@gmail.com>
2019-12-11 11:44:34 +0800

    style change

--- frontend/src/components/AccountInfo.js
+++ frontend/src/components/AccountInfo.js
@@ -12,7 +12,7 @@ export default function AccountInfo({ account }) {
       <CardItem style={styles.accountInfo}>
         <Text style={styles.depositId}>{`ID ${id}`}</Text>
         <Text numberOfLines={1} style={styles.depositName}>{`${depositName}`}</Text>
-        <Text style={styles.interestRate}>{`current interest rate: ${interestRate}`}</Text>
+        <Text style={styles.interestRate}>{`current interest rate: ${interestRate} %`}</Text>
       </CardItem>
 
       <CardItem style={styles.balance}>
